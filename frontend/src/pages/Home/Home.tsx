18602d6452adea88a901341921ae7f00f837db22
bassuntrai <bassuntrai@gmail.com>
2024-05-20 12:53:47 +0545

    update:useStateHook

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import Color from "../../hooks/color"
 import ReactHook from "../../hooks/hook"
 
 const Home = () => {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState('bg-green-50');
 
   return (
     <div>
@@ -14,23 +14,28 @@ const Home = () => {
 
       <Color
         buttonName="Change To Blue"
+        buttonColor={"bg-blue-500"}
         colorChange={() => setColor("bg-blue-500")}
       />
 
       <Color
         buttonName="Change To Yellow"
         colorChange={() => setColor("bg-yellow-500")}
+        buttonColor={"bg-yellow-500"}
       />
       <Color
         buttonName="Change To Purple"
         colorChange={() => setColor("bg-purple-500")}
+        buttonColor={"bg-purple-500"}
       />
       <Color
         buttonName="Change To Red"
         colorChange={() => setColor("bg-red-500")}
+        buttonColor={"bg-red-500"}
       />
       <Color
         buttonName="Change To Pink"
+        buttonColor={"bg-pink-500"}
         colorChange={() => setColor("bg-pink-500")}
       />
     </div>
